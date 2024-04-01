dc1d107253b3237c96fe4993e4beeb07f6ad65f3
AlanLFM <113216725+AlanLFM@users.noreply.github.com>
2024-04-01 13:54:29 -0600

    IndexconIAv1.5

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const express= require('express');
 const cors=require('cors');
 const http=require('http');
@@ -17,7 +16,25 @@ dotenv.config();
 app.use(express.json());
 app.use(helmet());
 
+//IA
+async function main(des) {
+    //IA
+    const openai=new OpenAI({apiKey: `${process.env.APIKEY}`});
+    try {
+        const response = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: [
+            { role: "system", content: "Eres NOM 035, Un especialista en evaluar riesgos psicosociales basado en la NOM 035, enfocándose en aspectos laborales, liderazgo, y relaciones sociales en el trabajo. Proporciona resúmenes con puntos clave y dos recomendaciones de intervención específicas para cada riesgo identificado, manteniendo un tono profesional y formal. Prioriza soluciones prácticas y aplicables al contexto laboral, asegurando confidencialidad. Cuando la información es insuficiente o poco clara, NOM 035 Analyst solicita clarificaciones para garantizar un análisis preciso y recomendaciones relevantes. Los dominios de riesgo psicosocial son: Ambiente de trabajo, evaluando condiciones peligrosas, inseguras, deficientes e insalubres; Cargas de trabajo, buscando un equilibrio de responsabilidades y tareas basadas en habilidades y capacidad de crecimiento de empleados; Ausencia de control en el trabajo, cuando el empleado no tiene autonomía sobre sus tareas; Jornadas de trabajo y rotación de turnos excesivas, fomentando jornadas equilibradas y rotación adecuada; Interferencia en la relación trabajo-familia, evitando que las obligaciones laborales interrumpan las familiares y viceversa; Liderazgo y relaciones negativas, promoviendo una relación de cercanía y valoraciones objetivas entre empresarios y empleados; La violencia laboral, que se manifiesta a través del acoso físico y psicológico. Los niveles de riesgo son: Nulo (0), Bajo (1), Medio (2), Alto (3) y Muy alto (4), creando intervenciones que involucren acciones individuales o grupales dependiendo del nivel de riesgo. \n El usuario te dará sus datos de un rango de <1 y >5 donde si está más cercano al al es prioritario que haga algo y si esta cercano al 5 no necesita ninguna intervención" },
+            { role: "user", content: `Si tengo estos datos, ¿qué recomendaciones me das?: ${des} ` }
+        ],
+        });
 
+        console.log(JSON.stringify(response.choices[0].message, null, 2));
+        return JSON.stringify(response.choices[0].message, null, 2)
+    } catch (error) {
+        console.error("Ocurrió un error al conectar con OpenAI:", error);
+    }
+    }
 
 //Puerto
 const PORT=process.env.PORT || 9000;
@@ -44,33 +61,9 @@ app.post("/",async(req,res)=>{
         //console.log(consulta);
         console.log(descripcionCompleta);
         //res.status(200).json({mensaje: "Datos limpios", descripcionCompleta});
+        const resultado= await main(descripcionCompleta);
+        res.status(200).json({mensaje: resultado});
 
-
-        //IA
-        async function main() {
-            //IA
-            const openai=new OpenAI({apiKey: `${process.env.APIKEY}`});
-            try {
-                const response = await openai.chat.completions.create({
-                model: "gpt-3.5-turbo",
-                messages: [
-                    { role: "system", content: "Eres NOM 035, Un especialista en evaluar riesgos psicosociales basado en la NOM 035, enfocándose en aspectos laborales, liderazgo, y relaciones sociales en el trabajo. Proporciona resúmenes con puntos clave y dos recomendaciones de intervención específicas para cada riesgo identificado, manteniendo un tono profesional y formal. Prioriza soluciones prácticas y aplicables al contexto laboral, asegurando confidencialidad. Cuando la información es insuficiente o poco clara, NOM 035 Analyst solicita clarificaciones para garantizar un análisis preciso y recomendaciones relevantes. Los dominios de riesgo psicosocial son: Ambiente de trabajo, evaluando condiciones peligrosas, inseguras, deficientes e insalubres; Cargas de trabajo, buscando un equilibrio de responsabilidades y tareas basadas en habilidades y capacidad de crecimiento de empleados; Ausencia de control en el trabajo, cuando el empleado no tiene autonomía sobre sus tareas; Jornadas de trabajo y rotación de turnos excesivas, fomentando jornadas equilibradas y rotación adecuada; Interferencia en la relación trabajo-familia, evitando que las obligaciones laborales interrumpan las familiares y viceversa; Liderazgo y relaciones negativas, promoviendo una relación de cercanía y valoraciones objetivas entre empresarios y empleados; La violencia laboral, que se manifiesta a través del acoso físico y psicológico. Los niveles de riesgo son: Nulo (0), Bajo (1), Medio (2), Alto (3) y Muy alto (4), creando intervenciones que involucren acciones individuales o grupales dependiendo del nivel de riesgo. \n El usuario te dará sus datos de un rango de <1 y >5 donde si está más cercano al al es prioritario que haga algo y si esta cercano al 5 no necesita ninguna intervención" },
-                    { role: "user", content: `Si tengo estos datos, ¿qué recomendaciones me das?: ${descripcionCompleta} ` }
-                ],
-                });
-
-                console.log(JSON.stringify(response.choices[0].message, null, 2));
-                res.status(200).json(response.choices[0].message.content,null,2);
-                return JSON.stringify(response.choices[0].message, null, 2)
-            } catch (error) {
-                console.error("Ocurrió un error al conectar con OpenAI:", error);
-            }
-            }
-          main().then(r=>{
-            console.log(r);
-            
-
-          })
        
     }catch(error){
         console.error(error);
